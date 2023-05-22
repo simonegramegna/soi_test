137989c275c941fabebbae1524cc637618ebb07c
simo_lenovo <gramegna.simone@gmail.com>
2023-05-22 17:34:21 +0200

    aggiunta kb da finire

---
+++ public/prolog/kb.pl
@@ -0,0 +1,9 @@
+correct_answer(cfu_1, 'dog').
+correct_answer(cfu_2, 'chickie').
+correct_answer(cfu_2, 'chick').
+correct_answer(cfu_3, 'duck').
+correct_answer(cfu_4, 'cup').
+correct_answer(cfu_5, 'hammer').
+correct_answer(cfu_6, 'shoe').
+correct_answer(cfu_7, 'frog').
+correct_answer(cfu_8, 'butterfly').
