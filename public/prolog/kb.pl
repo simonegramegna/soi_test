f67c0262469dec426d813eb29d868b1c14786ec4
simo_lenovo <gramegna.simone@gmail.com>
2023-05-23 09:51:12 +0200

    completata kb

--- public/prolog/kb.pl
+++ public/prolog/kb.pl
@@ -24,3 +24,28 @@ correct_answer(cfc_5, '8').
 correct_answer(cfc_6, '2').
 correct_answer(cfc_7, '1').
 correct_answer(cfc_8, '6').
+
+% module CFT
+correct_answer(cft_1, '3').
+correct_answer(cft_2, '5').
+correct_answer(cft_3, '4').
+correct_answer(cft_4, '2').
+correct_answer(cft_5, '1').
+correct_answer(cft_6, '3').
+correct_answer(cft_7, '5').
+
+% module CMR
+correct_answer(cmr_1, '3').
+correct_answer(cmr_2, '2').
+correct_answer(cmr_3, '3').
+correct_answer(cmr_4, '1').
+correct_answer(cmr_5, '2').
+correct_answer(cmr_6, '3').
+correct_answer(cmr_7, '3').
+
+% module CMS
+correct_answer(cms_1, '4').
+correct_answer(cms_2, '2').
+correct_answer(cms_3, '3').
+correct_answer(cms_4, '1').
+correct_answer(cms_5, '2').
