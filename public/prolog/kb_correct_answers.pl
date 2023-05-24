4c470c2af40f50462a677724378e660d9e1b75f8
simo_lenovo <gramegna.simone@gmail.com>
2023-05-24 18:33:39 +0200

    aggiunti altri test

--- public/prolog/kb_correct_answers.pl
+++ public/prolog/kb_correct_answers.pl
@@ -106,3 +106,76 @@ correct_answer(cms, 3, '3').
 correct_answer(cms, 4, '1').
 correct_answer(cms, 5, '4').
 correct_answer(cms, 6, '2').
+
+% module EFU
+correct_answer(efu, 1, '5').
+correct_answer(efu, 2, '2').
+correct_answer(efu, 3, '3').
+correct_answer(efu, 4, '1').
+correct_answer(efu, 5, '5').
+correct_answer(efu, 6, '2').
+correct_answer(efu, 7, '3').
+
+
+% module NST First Level
+correct_answer(nst_1, 1, 'dog').
+correct_answer(nst_1, 2, 'saw').
+correct_answer(nst_1, 3, 'many').
+correct_answer(nst_1, 4, 'came').
+correct_answer(nst_1, 5, 'line').
+correct_answer(nst_1, 6, 'good').
+
+
+% module NST Second Level
+correct_answer(nst_2, 1, 'the').
+correct_answer(nst_2, 1, 'girl').
+correct_answer(nst_2, 1, 'is').
+correct_answer(nst_2, 1, 'very').
+correct_answer(nst_2, 1, 'pretty').
+
+correct_answer(nst_2, 2, 'he').
+correct_answer(nst_2, 2, 'found').
+correct_answer(nst_2, 2, 'a').
+correct_answer(nst_2, 2, 'house').
+correct_answer(nst_2, 2, 'by').
+correct_answer(nst_2, 2, 'the').
+correct_answer(nst_2, 2, 'school').
+
+correct_answer(nst_2, 3, 'black').
+correct_answer(nst_2, 3, 'white').
+correct_answer(nst_2, 3, 'and').
+correct_answer(nst_2, 3, 'brown').
+correct_answer(nst_2, 3, 'are').
+correct_answer(nst_2, 3, 'good').
+correct_answer(nst_2, 3, 'colors').
+
+correct_answer(nst_2, 4, 'to').
+correct_answer(nst_2, 4, 'feel').
+correct_answer(nst_2, 4, 'glad').
+correct_answer(nst_2, 4, 'and').
+correct_answer(nst_2, 4, 'happy').
+correct_answer(nst_2, 4, 'is').
+correct_answer(nst_2, 4, 'a').
+correct_answer(nst_2, 4, 'fun').
+correct_answer(nst_2, 4, 'trip').
+correct_answer(nst_2, 4, 'for').
+correct_answer(nst_2, 4, 'people').
+
+
+% module MSU
+correct_answer(msu_auditory, 1, '386').
+correct_answer(msu_auditory, 2, '7492').
+correct_answer(msu_auditory, 3, '53186').
+
+correct_answer(msu_visual, 1, '251').
+correct_answer(msu_visual, 2, '7296').
+correct_answer(msu_visual, 3, '41375').
+
+% module MSS 
+correct_answer(mss_auditory, 1, '715').
+correct_answer(mss_auditory, 2, '4968').
+correct_answer(mss_auditory, 3, '13572').
+
+correct_answer(mss_visual, 1, '475').
+correct_answer(mss_visual, 2, '3948').
+correct_answer(mss_visual, 3, '25869').
