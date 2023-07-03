3f0d62b58c1f7b2b6c85c3940874cfdaa18f0a0e
Alberto G. Valerio <albertogvalerio@gmail.com>
2023-07-03 21:09:57 +0200

    get score fixing

--- my_prolog_module/index.pl
+++ my_prolog_module/index.pl
@@ -1,16 +1,14 @@
 :- use_module(utils, [ get_weighted_score/3, write_test_result_to_file/3, check_range/4 ]).
-:- use_module(kb_soi_abilities, [ ability/2 ]).
 :- use_module(kb_fake_results, [ test_result/2 ]).
 :- use_module(library(lists)).
 
 
 % It returs the Score obtained at a given ability test set.
-get_score_result([], [], 0, _).
-get_score_result([Answers|Tail], [Test|Tests], Result, Filename) :-
+get_score_result([], [], _, 0, _).
+get_score_result([Answers|Tail], [Test|Tests], SoiAbility, Result, Filename) :-
     get_weighted_score(Answers, Test, WeightedScore),
-    get_score_result(Tail, Tests, Result1, SoiAbility),
+    get_score_result(Tail, Tests, SoiAbility, Result1, Filename),
     Result is Result1 + WeightedScore,
-    ability(SoiAbility, [Test|Tests]), !,
     write_test_result_to_file(Filename, SoiAbility, Result).
 
 
@@ -23,4 +21,3 @@ get_avg_result(TestName, Score, Result) :-
     LowerBound is Min + Interval,
     UpperBound is Max - Interval,
     check_range(LowerBound, UpperBound, Score, Result).
-    
